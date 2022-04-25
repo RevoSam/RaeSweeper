8a294078990f009e38dbb2e3add65f6b9d2262e2
RevoSam <n-sami-m@hotmail.fr>
2022-04-25 13:10:30 -0400

    1st Version

---
+++ Cell.js
@@ -0,0 +1,87 @@
+function Cell(i, j, w){
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
+    this.bomb = false;
+    this.revealed = false;
+    this.neighborCount = 0;
+
+  }
+
+  Cell.prototype.show = function(){
+    if (this.revealed)
+    {   
+        if (this.bomb)
+        {
+            fill(100);
+            ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, + this.w * 0.5);
+        }
+        else{
+            fill(200);
+            rect(this.x, this.y, this.w, this.w);
+            if (this.neighborCount > 0)
+            {
+                textAlign(CENTER);
+                fill(0);
+                text(this.neighborCount, this.x + this.w*0.5, this.y + this.w*0.75);
+            }
+           
+        }
+    }
+    noFill();
+    rect(this.x, this.y, this.w, this.w);
+  }
+
+  Cell.prototype.contains = function (x, y){
+      return x > this.x && x < (this.x + this.w) && y > this.y && y < this.y + this.w;
+  }
+
+  Cell.prototype.reveal = function()
+  {
+    this.revealed = true;  
+    if (this.neighborCount == 0 && !this.bomb)
+      {
+          this.showBlank();
+      }
+      
+  }
+
+  Cell.prototype.showBlank = function()
+  {
+    for(var i = -1; i <= 1; i++)
+    {
+        for (var j = -1; j <= 1; j++)
+        {
+            if ((this.i + i ) > -1 && (this.i + i) < rows && (this.j + j > -1) && (this.j+j<cols))
+            {
+                var neighbor = grid[this.i + i][this.j + j];
+                if (!neighbor.bomb && !neighbor.revealed)
+                    neighbor.reveal();
+            }
+                
+        }
+    }
+  }
+
+  Cell.prototype.countNeighbors = function(){
+    // if (this.bomb)
+    //     return -1;
+    var total = 0;
+    
+    for(var i = -1; i <= 1; i++)
+    {
+        for (var j = -1; j <= 1; j++)
+        {
+            if ((this.i + i ) > -1 && (this.i + i) < rows && (this.j + j > -1) && (this.j+j<cols))
+            {
+                var neighbor = grid[this.i + i][this.j + j];
+                if (neighbor.bomb)
+                    total++;
+            }
+                
+        }
+    }
+    this.neighborCount = total;
+  }
---
+++ sketch.js
@@ -0,0 +1,74 @@
+var grid;
+var cols;
+var rows;
+var w = 20;
+var totalBombs = 10;
+
+function setup() {
+  createCanvas(201, 201);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = create2D(rows, cols);
+  for (let index = 0; index < rows; index++) {
+    for (let jndex = 0; jndex < cols; jndex++) {
+      grid[index][jndex] = new Cell(index, jndex, w);
+    }
+  }
+  var index = 0;
+  while (index < totalBombs) {
+    var i = floor(random(rows));
+    var j = floor(random(cols));
+    if (grid[i][j].bomb != true)
+    {
+      grid[i][j].bomb = true;
+      index++;
+    }
+  }
+  for (let index = 0; index < rows; index++) {
+    for (let jndex = 0; jndex < cols; jndex++) {
+      grid[index][jndex].countNeighbors();
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  for (let index = 0; index < rows; index++) {
+    for (let jndex = 0; jndex < cols; jndex++) {
+      grid[index][jndex].show();
+    }
+  }
+}
+
+function mousePressed() {
+  for (let index = 0; index < rows; index++) {
+    for (let jndex = 0; jndex < cols; jndex++) {
+      if (grid[index][jndex].contains(mouseX, mouseY))
+      {
+        grid[index][jndex].reveal();
+        if (grid[index][jndex].bomb)
+          gameOver();
+      }
+    }
+  }
+}
+
+function gameOver()
+{
+  for (let index = 0; index < rows; index++) {
+    for (let jndex = 0; jndex < cols; jndex++) {
+      grid[index][jndex].revealed = true;
+    }
+  }
+}
+
+function create2D(rows, cols)
+{
+  var arr = new Array(rows);
+  for (let index = 0; index < arr.length; index++) {
+    arr[index] = new Array(cols);
+  }
+  return arr;
+}
+
+
